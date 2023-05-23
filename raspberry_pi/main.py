126ab379be054e2c37c96da7f1908807ccec5cf8
Grin <asusgrin@gmail.com>
2023-05-23 15:02:33 +0300

    main.py

--- raspberry_pi/main.py
+++ raspberry_pi/main.py
@@ -21,31 +21,29 @@ GPIO.setmode(GPIO.BCM)
 app = Flask(__name__)
 limiter = Limiter(app, key_func=get_remote_address)
 
-@app.route("/", methods=['GET','POST'])
 @limiter.limit("1/second", override_defaults=False)
+
 def index():
     if request.method == 'POST':
         Relay = int(request.form.get('Relay'))
         Status = int(request.form.get('Status'))
-        print({'relay id ': Relay})
-        print('--------------------------------')
-        print({'relay status': Status})
         GPIO.setup(Relay,GPIO.OUT)
-        data = {'message': 'Done', 'code': 'SUCCESS','id': str(Relay)}
-
+        data = {'relay_id': Relay, 'status': Status, 'message': 'Done'}
         try:
             if Status == 0:
                 GPIO.output(Relay,GPIO.HIGH)
                 time.sleep(1)
-                return make_response(jsonify(data,'ON'), 200)
+                return make_response(jsonify(data, 200))
             if Status == 1:
                 GPIO.output(Relay,GPIO.LOW)
                 time.sleep(1)
-                return make_response(jsonify(data,'OFF'), 200)
+                return make_response(jsonify(data, 200))
         except Exception as inst:
            return make_response(jsonify(inst), 404)
+           # return( type(inst), inst.args, inst)
 
     else:
         return make_response(jsonify("Error Not Valid Request"), 404)
 if __name__ == "__main__":
     app.run(debug="True", host="0.0.0.0")
+
